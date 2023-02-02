0a9e0adbcebc8f138dc1bb11d6ec946253fa8931
kvelraj_concord <kvelraj@concord.net>
2023-02-02 10:15:13 +0530

    source code

---
+++ HTML/program.js
@@ -0,0 +1,12 @@
+// Number -> Any Numerical value from positive, negative, decimal, fraction
+
+// To create variable (Temp Storage)
+// syntax 
+// var variable_name = value;
+
+// var -> keyword
+// variable_name -> any valid meaning Text, it should be a-z, A-Z, 0-9, _, $
+// value -> Input data
+
+var user_age = 25;
+console.log(user_age);

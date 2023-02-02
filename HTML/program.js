512aac1c0f72f31af82aa4e5afb4eb254d4d2389
kvelraj_concord <kvelraj@concord.net>
2023-02-02 10:46:30 +0530

    source code

--- HTML/program.js
+++ HTML/program.js
@@ -10,3 +10,21 @@
 
 var user_age = 25;
 console.log(user_age);
+
+// String -> Any text value which is typed inside '' or ""
+
+var email_id = "karthick@gmail.com";
+console.log(email_id);
+
+// Boolean -> true or false
+
+var result = true;
+console.log(result);
+
+// undefined -> A variable name will be created but its value will not be assingned 
+var future_value;
+console.log(future_value);
+
+email_id = "k@outlook.com";
+
+console.log(email_id);

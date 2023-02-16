7ce8eea4aa490c87f32e57863429286fb5a1fef6
kvelraj_concord <kvelraj@concord.net>
2023-02-16 10:17:16 +0530

    source code

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import HeaderComponent from "./components/header";
+import FooterComponent from "./components/footer";
 
 const App = () => {
   return(
     <div>
+      <HeaderComponent></HeaderComponent>
       <h1>Good Morning!</h1>
+      <FooterComponent></FooterComponent>
     </div>
   )
 }
---
+++ firstapp/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return(
+    <div>
+      <h1>Header of the Page</h1>
+    </div>
+  )
+}
+
+export default HeaderComponent;

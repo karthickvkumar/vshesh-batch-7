1e18e4bd5cfa88f7ac232885e905e179cf5fd1a5
kvelraj_concord <kvelraj@concord.net>
2023-02-21 10:51:54 +0530

    source code

--- firstapp/src/pages/contact.js
+++ firstapp/src/pages/contact.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import HeaderComponent from "../components/header";
 
 const ContactPage = () => {
+
+  // creating a state variable 
+  // const [variableName, functionName] = useState(initalData);
+
+  const [form, accountSubmition] = useState({
+    username: "",
+    emailId : "",
+    password : "",
+    gender: "",
+    date_of_birth: "",
+    education: "",
+    address: ""
+  });
+
+  const submitForm = () => {
+    // print -> stateVaiable_vaiableName
+    console.log(form);
+  }
+
+  const handleInput = (event) => {
+    // stateVariable_functionName({...stateVaiable_varaibleName, [event.target.name] : event.target.value })
+    accountSubmition({...form, [event.target.name] : event.target.value });
+  }
+
   return(
     <div>
       <HeaderComponent></HeaderComponent>
@@ -9,29 +33,30 @@ const ContactPage = () => {
       <div>
         <div className="space">
           <label className="lable">Enter your Name :</label>
-          <input type="text" placeholder="Enter your name" />
+          <input type="text" placeholder="Enter your name" onChange={handleInput} name="username" />
         </div>
         <div className="space">
           <label className="lable">Enter your Email ID :</label>
-          <input type="text" placeholder="Enter your email id" />
+          <input type="text" placeholder="Enter your email id" onChange={handleInput} name="emailId" />
         </div>
         <div className="space">
           <label className="lable">Enter your Password :</label>
-          <input type="text" placeholder="Enter your passord" />
+          <input type="password" placeholder="Enter your passord" onChange={handleInput} name="password"/>
         </div>
         <div className="space">
           <label className="lable">Select your Gender :</label>
-          <input type="radio" />Male
-          <input type="radio" />Female
-          <input type="radio" />Others
+          <input type="radio" onChange={handleInput} name="gender" value="Male"/>Male
+          <input type="radio" onChange={handleInput} name="gender" value="Female"/>Female
+          <input type="radio" onChange={handleInput} name="gender" value="Others"/>Others
         </div>
         <div className="space">
           <label className="lable">Select your DOB :</label>
-          <input type="date" />
+          <input type="date" onChange={handleInput} name="date_of_birth"/>
         </div>
         <div className="space">
           <label className="lable">Select your Education :</label>
-          <select>
+          <select onChange={handleInput} name="education">
+            <option disabled selected>Select any value</option>9
             <option>B.E Compter Science</option>
             <option>B.E Mechanical</option>
             <option>B.Tech IT</option>
@@ -42,10 +67,10 @@ const ContactPage = () => {
         </div>
         <div className="space">
           <label className="lable">Enter your Address :</label>
-          <textarea placeholder="Enter your address"></textarea>
+          <textarea placeholder="Enter your address" onChange={handleInput} name="address"></textarea>
         </div>
         <div className="space">
-          <button>Submit Form</button>
+          <button onClick={() => submitForm()}>Submit Form</button>
         </div>
       </div>
     </div>

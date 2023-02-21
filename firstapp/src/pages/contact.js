0c0840fa23f4356942b9854dad0150cfaed3def4
kvelraj_concord <kvelraj@concord.net>
2023-02-21 10:16:38 +0530

    source code

--- firstapp/src/pages/contact.js
+++ firstapp/src/pages/contact.js
@@ -6,6 +6,48 @@ const ContactPage = () => {
     <div>
       <HeaderComponent></HeaderComponent>
       <h1>Welcome to Contact Page</h1>
+      <div>
+        <div className="space">
+          <label className="lable">Enter your Name :</label>
+          <input type="text" placeholder="Enter your name" />
+        </div>
+        <div className="space">
+          <label className="lable">Enter your Email ID :</label>
+          <input type="text" placeholder="Enter your email id" />
+        </div>
+        <div className="space">
+          <label className="lable">Enter your Password :</label>
+          <input type="text" placeholder="Enter your passord" />
+        </div>
+        <div className="space">
+          <label className="lable">Select your Gender :</label>
+          <input type="radio" />Male
+          <input type="radio" />Female
+          <input type="radio" />Others
+        </div>
+        <div className="space">
+          <label className="lable">Select your DOB :</label>
+          <input type="date" />
+        </div>
+        <div className="space">
+          <label className="lable">Select your Education :</label>
+          <select>
+            <option>B.E Compter Science</option>
+            <option>B.E Mechanical</option>
+            <option>B.Tech IT</option>
+            <option>B.SC Computer Science</option>
+            <option>M.Tech Cloud</option>
+            <option>M.Phil</option>
+          </select>
+        </div>
+        <div className="space">
+          <label className="lable">Enter your Address :</label>
+          <textarea placeholder="Enter your address"></textarea>
+        </div>
+        <div className="space">
+          <button>Submit Form</button>
+        </div>
+      </div>
     </div>
   )
 }

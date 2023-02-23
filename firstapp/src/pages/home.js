40fa66214115dfd352a0e2869a2fa95a0b0a75e9
kvelraj_concord <kvelraj@concord.net>
2023-02-23 10:13:04 +0530

    source code

--- firstapp/src/pages/home.js
+++ firstapp/src/pages/home.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderComponent from "../components/header";
+import axios from "axios";
 
 const HomePage = () => {
+
+  useEffect(() => {
+    listingUserProfile();
+  }, []);
+
+  const [userProfile, LoadAllProfile] = useState([]);
+
+  const listingUserProfile = () => {
+    const url = "https://reqres.in/api/users?page=2";
+
+    axios.get(url)
+        .then((response) => {
+          const serverData = response.data;
+          LoadAllProfile(serverData.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Something went wrong pls try again later");
+        })
+  }
+
   return(
     <div>
       <HeaderComponent></HeaderComponent>
       <h1>Welcome to Home Page</h1>
+      <table className="customers">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Profile Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userProfile.map((value, index) => {
+              return(
+                <tr key={index}>
+                  <td>{value.id}</td>
+                  <td>{value.email}</td>
+                  <td>{value.first_name}</td>
+                  <td>{value.last_name}</td>
+                  <td>
+                    <img src={value.avatar} className="circle-image" />
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }

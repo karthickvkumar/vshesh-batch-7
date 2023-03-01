3947d861e401b9e406ef835592b6cbc6fae6ec39
kvelraj_concord <kvelraj@concord.net>
2023-03-01 10:01:41 +0530

    soruce code

--- node-app/app.js
+++ node-app/app.js
@@ -11,9 +11,32 @@ app.use(cors({
   origin: '*'
 }));
 
+var connection = mysql.createConnection({
+  host: "sql12.freemysqlhosting.net",
+  user: "sql12601321",
+  password: "DRvyHLYxWX",
+  database: "sql12601321",
+  port: 3306
+});
+
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+  else{
+    console.log("MYSQL Database is connected successfully");
+  }
+})
 
 
 var port = process.env.PORT || 4000;
 http.listen(port, () => {
   console.log("Node JS Server is running on Port 4000");
 }); 
+
+
+// Server: sql12.freemysqlhosting.net
+// Database Name: sql12601321
+// Username: sql12601321
+// Password: DRvyHLYxWX
+// Port number: 3306
